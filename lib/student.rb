23efbd84bad080580da6339f38dd4617333ad3a7
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-28 04:06:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,7 @@ class Student
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << self
+    binding.pry
   end
 
   # iterate over the array of hashes and create a new individual student using each hash.
