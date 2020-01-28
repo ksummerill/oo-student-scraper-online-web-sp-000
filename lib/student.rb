f6792cf3033ca7bdb80357afe90ad5244c7fd228
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-28 04:30:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,9 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
+    student_hash.each do |k, v| 
+      self.send(("#{k}="), v)
     @@all << self
-    # binding.pry
   end
 
   # iterate over the array of hashes and create a new individual student using each hash.
