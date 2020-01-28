fc866aa835dc179cc08669b812b81e2efd0a472a
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-28 04:01:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,9 @@ class Student
 
   # iterate over the array of hashes and create a new individual student using each hash.
   def self.create_from_collection(students_array)
-    Scraper.new(students_array)
+    students_array.each do |new_student|
+      Scraper.new(new_student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
