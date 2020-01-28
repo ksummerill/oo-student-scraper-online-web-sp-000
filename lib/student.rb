4cc4f359e8b7ae3aadaa1c0f88e05d3836d6da39
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-28 04:15:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
     students_array.each do |new_student|
       # binding.pry
       # first loop, new_student = {:name=>"Alex Patriquin", :location=>"New York, NY"}
-      new_student
+      Student.new(new_student)
     end
   end
 
